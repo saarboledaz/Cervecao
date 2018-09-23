be6ae535def4f494208328cd6f8f970cc9c25dc4
saarboledaz <sergioarboledazuluaga@gmail.com>
2018-09-23 10:50:28 -0500

    Add files via upload

---
+++ js/lib.js
@@ -0,0 +1,33 @@
+$(document).ready(() => {
+    const $navBar = $('#navbar');
+    const $navElements = $navBar.find('li');
+    $navElements.on('mouseenter', function (event) {
+        $(event.currentTarget).addClass('nav-active');
+        $(event.currentTarget).animate({ fontSize: '18px' }, 200);
+    });
+    $navElements.on('mouseleave', function (event) {
+        $(event.currentTarget).removeClass('nav-active');
+
+        $(event.currentTarget).animate({ fontSize: '14px' }, 0);
+    });
+    const $contactButton = $('.contact');
+    $contactButton.on('mouseenter', function (event) {
+        $(event.currentTarget).addClass('nav-active');
+        $(event.currentTarget).animate({ fontSize: '18px' }, 200);
+    });
+    $contactButton.on('mouseleave', function (event) {
+        $(event.currentTarget).removeClass('nav-active');
+        $(event.currentTarget).animate({ fontSize: '14px' }, 200);
+
+    });
+    $contactButton.on('click', function(){
+        const $contactW = $('#contactw');
+        if ($contactW.css('visibility').toLowerCase() == 'hidden'){
+            
+            $contactW.addClass('wActive');
+        }else{
+            $contactW.removeClass('wActive');
+             
+        }
+    });
+});

1ff52068e0a043b1670b0269ec25076cd097276c
Sergio Arboleda Zuluaga <sergioarboledazuluaga@gmail.com>
2019-02-02 22:23:20 -0500

    :p

--- js/lib.js
+++ js/lib.js
@@ -10,16 +10,7 @@ $(document).ready(() => {
 
         $(event.currentTarget).animate({ fontSize: '14px' }, 0);
     });
-    const $contactButton = $('.contact');
-    $contactButton.on('mouseenter', function (event) {
-        $(event.currentTarget).addClass('nav-active');
-        $(event.currentTarget).animate({ fontSize: '18px' }, 200);
-    });
-    $contactButton.on('mouseleave', function (event) {
-        $(event.currentTarget).removeClass('nav-active');
-        $(event.currentTarget).animate({ fontSize: '14px' }, 200);
 
-    });
     $('.tooltip').on('mouseenter', function(event){
         $('.tooltip').find('div').removeClass('refu').addClass('refa');
     });
